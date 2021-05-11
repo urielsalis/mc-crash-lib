996c7d7f9e13ee000e442b6b3092775f913baf72
urielsalis <uriel@urielsalis.me>
2021-05-11 11:42:27 +0200

    Use java 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,9 +30,9 @@ configure<JavaPluginConvention> {
 }
 tasks {
     compileKotlin {
-        kotlinOptions.jvmTarget = "1.8"
+        kotlinOptions.jvmTarget = "11"
     }
     compileTestKotlin {
-        kotlinOptions.jvmTarget = "1.8"
+        kotlinOptions.jvmTarget = "11"
     }
 }
