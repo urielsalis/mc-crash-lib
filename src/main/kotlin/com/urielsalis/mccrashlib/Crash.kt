47d8d3f248efe0688a417b8f4b53e65a94e0bde3
Marcono1234 <Marcono1234@users.noreply.github.com>
2021-10-12 16:52:47 +0200

    Improve Minecraft crash report info and deobfuscation API

--- src/main/kotlin/com/urielsalis/mccrashlib/Crash.kt
+++ src/main/kotlin/com/urielsalis/mccrashlib/Crash.kt
@@ -5,7 +5,10 @@ sealed class Crash {
     data class Minecraft(
         val modded: Boolean,
         val exception: String,
-        val minecraftVersion: String,
+        /** Minecraft version ID; `null` if unknown */
+        val minecraftVersion: String?,
+        /** Whether this is a client crash (`true`) or a dedicated server crash (`false`) */
+        val isClient: Boolean,
         val deobf: String?,
         val deobfException: String?
     ) : Crash()
